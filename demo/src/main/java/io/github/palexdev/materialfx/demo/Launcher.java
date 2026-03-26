808c0a28cb1dae112e2c5e78b5743a9d0f37009c
Chris <who7708@qq.com>
2026-03-27 00:48:09 +0800

    add Launcher

---
+++ demo/src/main/java/io/github/palexdev/materialfx/demo/Launcher.java
@@ -0,0 +1,14 @@
+package io.github.palexdev.materialfx.demo;
+
+/**
+ * 这个类不继承 Application，是一个普通的 Java 类
+ *
+ * @author Chris
+ * @version 1.0.0
+ * @since 2026/2/24
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Demo.launch(args);
+    }
+}
